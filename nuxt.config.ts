21761311bd63298dddb0c7d61066ab18f24325f7
pandeynishuu <pandeynishuuu@gmail.com>
2023-07-23 14:01:37 +0545

    login section completed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@element-plus/nuxt'],
+  modules: [
+    '@nuxtjs/tailwindcss'
+]
+// app:{
+//   head:{
+//     link:[
+//       {
+//         rel:"stylesheet",
+//         href:"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+//       }
+//     ],
+//     script:[
+//       {
+//         src:"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js",
+//         type:"text/javascript"
+//       }
+//     ]
+//   }
+// }
 })
